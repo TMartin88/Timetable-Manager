4b2b9849bc2d207ded1ed2bd8a442102dcd4f346
TMartin88 <tiernanmartin1@gmail.com>
2023-01-28 16:58:14 +0000

    SummerNote installed and setup

--- schedules/models.py
+++ schedules/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -33,9 +35,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def number_of_likes(self):
         return self.likes.count()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
