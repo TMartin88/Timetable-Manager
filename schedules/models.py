480af07593557eeed6615c3d4c0cd03462f4932f
TMartin88 <tiernanmartin1@gmail.com>
2023-01-28 16:17:46 +0000

    Setup Post Model

--- schedules/models.py
+++ schedules/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="schedules_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    timetable_pdf = CloudinaryField('pdf', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    notes = models.TextField()
+    fare_calculator = models.BooleanField()
+    change_direction = models.BooleanField()
+    other_fares = models.BooleanField()
+    leap_link = models.URLField()
+    go_app_link = models.URLField()
+    public_notes = models.TextField()
+    active_days = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='schedulespost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+    
+    def number_of_likes(self):
+        return self.likes.count()
