6295e43225af376670f052a630ee30e99fd9f73b
TMartin88 <tiernanmartin1@gmail.com>
2023-01-29 15:03:36 +0000

    Add auto slu and filter feature to posts

--- schedules/admin.py
+++ schedules/admin.py
@@ -6,5 +6,6 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
