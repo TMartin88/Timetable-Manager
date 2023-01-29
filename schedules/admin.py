18bd3989fdbff7d969b733236f945490da19878d
TMartin88 <tiernanmartin1@gmail.com>
2023-01-29 16:04:15 +0000

    Add action to approve to admin.py

--- schedules/admin.py
+++ schedules/admin.py
@@ -18,3 +18,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     search_fields = ('name', 'email', 'body')
     list_filter = ('approved', 'created_on')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
