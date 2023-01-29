b39994e7567b189a23288b146867dd554c39e7b4
TMartin88 <tiernanmartin1@gmail.com>
2023-01-29 15:11:32 +0000

    Polish Post backend presentation with search and list display

--- schedules/admin.py
+++ schedules/admin.py
@@ -6,6 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content',)
