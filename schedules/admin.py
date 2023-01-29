c70ed68b722c357728b8d106731d54e682659ec1
TMartin88 <tiernanmartin1@gmail.com>
2023-01-29 15:53:47 +0000

    Add Comments to admin.py

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -11,3 +11,10 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    search_fields = ('name', 'email', 'body')
+    list_filter = ('approved', 'created_on')
