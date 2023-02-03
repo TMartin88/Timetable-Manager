73a055997b53942fba61e1449d56e0e1c5e0fbc0
TMartin88 <tiernanmartin1@gmail.com>
2023-02-03 22:24:41 +0000

    Full Text search django.contrib.postgres in installed apps.

--- codeschedule/settings.py
+++ codeschedule/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
+    'django.contrib.postgres',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
--- user_search/views.py
+++ user_search/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "search_results.html"
+    paginate_by = 6
