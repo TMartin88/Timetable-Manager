a2ab9f90d884198a301836e1b884ba4bc93bd440
TMartin88 <tiernanmartin1@gmail.com>
2023-02-05 17:33:46 +0000

    Search results page changed to card style

--- user_search/views.py
+++ user_search/views.py
@@ -6,11 +6,14 @@ from django.db.models import Q
 
 class SearchResultsList(generic.ListView):
     '''
-    This is from https://testdriven.io/blog/django-search/
+    This is from https://testdriven.io/blog/django-search/ for the fancy search
+    and https://stackoverflow.com/questions/51631651/why-use-get-context-data-self-kwargs-and-super
+    for the context
     '''
     model = Post
     context_object_name = "posts"
     template_name = "search_results.html"
+    paginate_by = 6
 
     def get_queryset(self):
         query = self.request.GET.get("q")
