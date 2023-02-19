3a9aa860494c1dc73c1278d191ee28fb0a33130d
TMartin88 <tiernanmartin1@gmail.com>
2023-02-19 19:55:58 +0000

    Amend Table of posts on index page

--- schedules/views.py
+++ schedules/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.http import JsonResponse
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm
 
 
